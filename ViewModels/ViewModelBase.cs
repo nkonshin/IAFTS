ecf4b3eb0510b8bf741cb10b619cf54c86ae85f3
nkonshin <47538881+nkonshin@users.noreply.github.com>
2025-05-21 01:03:06 +0500

    main version

---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using ReactiveUI;
+
+namespace IAFTS.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
---
+++ Views/AboutUsWindow.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace IAFTS;
+
+public partial class AboutUsWindow : Window
+{
+    public AboutUsWindow()
+    {
+        InitializeComponent();
+    }
+}
