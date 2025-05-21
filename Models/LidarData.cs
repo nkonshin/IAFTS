abf387727ec1e67d5e4028ba20623794a23a9e42
nkonshin <47538881+nkonshin@users.noreply.github.com>
2025-05-21 19:27:31 +0500

    added Shp, Csv, Image file paths

--- Models/LidarData.cs
+++ Models/LidarData.cs
@@ -6,8 +6,11 @@ namespace IAFTS.Models
     public class LidarData
     {
         public string? LasFilePath { get; set; }    // Путь к файлу LAS
-        public string? TiffFilePath { get; set; }   // Путь к файлу TIFF
-        public string? OutputPath { get; set; }     // Путь для сохранения результатов
+        public string? TiffFilePath { get; set; }   // Путь к файлу TIFF (для нейронки)
+        public string? ShpFilePath { get; set; }    // Путь к shape-файлу (выход нейросети)
+        public string? CsvFilePath { get; set; }    // Путь к итоговому CSV (выход скрипта поиска)
+        public string? ImageFilePath { get; set; }  // Путь к файлу изображения (для нейронки)
+        public string? OutputPath { get; set; }     // Путь для сохранения результатов (выбирается пользователем)
         public List<Tree>? Trees { get; set; }      // Список обнаруженных деревьев
 
         public LidarData()
